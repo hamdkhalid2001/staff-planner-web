1b193f3a4a79207ea7bf05fb4a025d9f67fe8de8
Syed Hamd <shamd@alec.ae>
2025-09-16 10:01:25 +0500

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@progress/kendo-theme-fluent/dist/all.scss";
 import "./globals.css";
 
 const geistSans = Geist({
